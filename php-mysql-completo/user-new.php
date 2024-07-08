d700c0ac8f098985626cab0ab22b93ac3400019d
Werbster <wmteixeira.88@gmail.com>
2024-07-08 16:58:37 -0300

    [REACT] Atualizando títulos.

--- php-mysql-completo/user-new.php
+++ php-mysql-completo/user-new.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar novo usuário</title>
+    <title>Cadastrar Novo Usuário</title>
     <link rel="stylesheet" href="estilos/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
